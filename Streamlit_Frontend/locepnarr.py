7de4bd5ec1de86fc07b619275a5ec664fedc978c
swathi <sharma.sw@northeastern.edu>
2022-04-15 04:57:30 -0400

    storm details

--- Streamlit_Frontend/locepnarr.py
+++ Streamlit_Frontend/locepnarr.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 from nearbyloc import locfinder
 def findepnarr(location,begintime,endtime):
-    df = pd.read_csv("StormEvents_details.csv")
+    df = pd.read_csv("Streamlit_Frontend/StormEvents_details.csv")
     if df.loc[(df['BEGIN_LOCATION']==location) & (df['BEGIN_DATE_TIME'] == begintime) & (df['END_DATE_TIME'] == endtime)].shape[0] >= 1:
         int_df = df[(df['BEGIN_LOCATION'] == location) & (df['BEGIN_DATE_TIME'] == begintime) & (df['END_DATE_TIME'] == endtime)]
         #print(int_df["EPISODE_NARRATIVE"].loc[int_df.index[0]])
